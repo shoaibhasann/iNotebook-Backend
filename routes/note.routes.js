27a57d5dd2f789f96a8cca7a69de6f58aacf08f4
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-01 23:36:06 +0530

    Create routes for notes

---
+++ routes/note.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import isLoggedIn from "../middlewares/auth .middleware.js";
+import { allNotes, createNote, deleteNote, updateNote } from "../controllers/note.controllers.js";
+
+const router = Router();
+
+router.post('/', isLoggedIn, createNote);
+router.get('/', isLoggedIn, allNotes);
+router.put('/:id', isLoggedIn, updateNote);
+router.delete('/:id', isLoggedIn, deleteNote);
+
+
+export default router;
